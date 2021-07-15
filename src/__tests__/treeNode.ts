d053cb71a6411a9db2b066af389d7418e7f028f6
Jerell James <jerell1998@gmail.com>
2021-07-15 16:08:03 +0100

    tree

--- src/__tests__/treeNode.ts
+++ src/__tests__/treeNode.ts
@@ -2,14 +2,43 @@ import TreeNode from '../treeNode'
 
 describe('constructor', () => {
   it('should set the value of the node', () => {
-    const tn = new TreeNode(6, 'treenode')
+    const tn = new TreeNode({ value: 6, name: 'treenode' })
 
     expect(tn.value).toBe(6)
   })
 
-  it('should set the name of the node', () => {
-    const tn = new TreeNode(6, 'treenode')
+  it('should have a default name', () => {
+    const tn = new TreeNode()
 
     expect(tn.name).toBe('treenode')
+  })
+
+  it('should set the name of the node', () => {
+    const tn = new TreeNode({ value: 6, name: 'namednode' })
+
+    expect(tn.name).toBe('namednode')
+  })
+
+  it('should default destination of `null`', () => {
+    const tn = new TreeNode()
+
+    expect(tn.destination).toBeNull()
+  })
+
+  it('should have no sources by default', () => {
+    const tn = new TreeNode()
+
+    expect(tn.sources.length).toBe(0)
+  })
+})
+
+describe('sources', () => {
+  it('should add a node to the list of sources', () => {
+    const tn = new TreeNode()
+    const tnChild = new TreeNode()
+
+    tn.addChildNode(tnChild)
+
+    expect(tn.sources).toContain(tnChild)
   })
 })
--- src/treeNode.ts
+++ src/treeNode.ts
@@ -1,13 +1,22 @@
+interface ITNode {
+  value?: number
+  name: string
+}
+
 export default class TreeNode {
   value: number
   name: string
   sources: TreeNode[]
   destination: TreeNode | null
 
-  constructor(value: number, name: string) {
-    this.value = value
-    this.name = name
+  constructor(props: ITNode = { name: 'treenode' }) {
+    this.value = props.value || 0
+    this.name = props.name
     this.sources = []
     this.destination = null
+  }
+
+  addChildNode(node: TreeNode) {
+    this.sources.push(node)
   }
 }
